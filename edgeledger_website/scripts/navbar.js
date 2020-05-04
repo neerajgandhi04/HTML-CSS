9c4af4fcceab65218d55094d29149675c1b87b32
Teodora321 <teodoraakehaiovaa@gmail.com>
2020-05-04 13:38:39 +0300

    transparent navbar

--- edgeledger_website/scripts/navbar.js
+++ edgeledger_website/scripts/navbar.js
@@ -1,13 +1,18 @@
 const navbar = document.getElementById('navbar');
 let scrolled = false;
 
-window.onscroll = ()=> {
+window.onscroll = function () {
   if (window.pageYOffset > 100) {
     navbar.classList.remove('top');
     if (!scrolled) {
-      navbar.style.transform = 'translateY(-70px)'
+      navbar.style.transform = 'translateY(-70px)';
     }
+    setTimeout(function () {
+      navbar.style.transform = 'translateY(0)';
+      scrolled = true;
+    }, 200)
   } else {
-    navbar.classList.add('top')
+    navbar.classList.add('top');
+    scrolled = false
   }
-};
+}
